d126bb13cfa0002565baeeb0bbf596682c6664e2
mihad <mihad>
2002-02-01 17:30:49 +0000

    *** empty log message ***

--- old_stuff/pci_target/pci_stat.v
+++
@@ -1,8 +0,0 @@
-// pci termination status encoding definitions
-`define WAIT 4'h0 
-`define TRANSFERED 4'h1
-`define DISCONNECT_WO_DATA 4'h2
-`define DISCONNECT_W_DATA 4'h3
-`define TABORT 4'h4
-`define MABORT 4'h5
-`define RETRY 4'h6
--- old_stuff/wb_slave/READ_ME.txt
+++
@@ -1,5 +0,0 @@
-wb_slave.v file:
-File includes WISHBONE slave module for PCI bridge project.
-
-delayed_write_reg.v file
-File include just one register that is used for delayed writes ( at this time only for configuration writes)
