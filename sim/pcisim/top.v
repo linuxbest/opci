90a246df9cd28c063c128051440b60f4192ee7d3
hugang <hugang@hugang-macbook.(none)>
2008-12-17 17:23:23 +0800

    modified:   ../../sim/pcisim/top.v
            adding 64bit.

--- sim/pcisim/top.v
+++ sim/pcisim/top.v
@@ -1,129 +1,146 @@
-module top
-(
-    CLK,
-    AD,
-    CBE,
-    RST,
-    INTA,
-    REQ,
-    GNT,
-    FRAME,
-    IRDY,
-    IDSEL,
-    DEVSEL,
-    TRDY,
-    STOP,
-    PAR,
-    PERR,
-    SERR
-);
+module top (/*AUTOARG*/
+   // Outputs
+   REQ, SERR,
+   // Inouts
+   AD, CBE, INTA, FRAME, IRDY, DEVSEL, TRDY, STOP, PAR,
+   PERR, AD64, CBE64, PAR64, ACK64, REQ64,
+   // Inputs
+   CLK, RST, GNT, IDSEL
+   );
 
-parameter oe_act = 1'b0 ;
+   parameter oe_act = 1'b0 ;
+   
+   input           CLK ;
+   inout [31:0]    AD ;
+   inout [3:0] 	   CBE ;
+   input           RST ;
+   inout           INTA ;
+   output          REQ ;
+   input           GNT ;
+   inout           FRAME ;
+   inout           IRDY ;
+   input           IDSEL ;
+   inout           DEVSEL ;
+   inout           TRDY ;
+   inout           STOP ;
+   inout           PAR ;
+   inout           PERR ;
+   output          SERR ;
 
-input           CLK ;
-inout   [31:0]  AD ;
-inout   [3:0]   CBE ;
-input           RST ;
-inout           INTA ;
-output          REQ ;
-input           GNT ;
-inout           FRAME ;
-inout           IRDY ;
-input           IDSEL ;
-inout           DEVSEL ;
-inout           TRDY ;
-inout           STOP ;
-inout           PAR ;
-inout           PERR ;
-output          SERR ;
+   inout [31:0]    AD64;
+   inout [3:0] 	   CBE64;
+   inout 	   PAR64;
+   inout 	   ACK64;
+   inout 	   REQ64;
+   
+   wire [31:0] 	   AD_out ;
+   wire [31:0] 	   AD_en ;
+   wire [31:0] 	   AD_in   ;
 
-wire    [31:0]  AD_out ;
-wire    [31:0]  AD_en ;
+   wire [31:0] 	   AD64_out ;
+   wire [31:0] 	   AD64_en ;
+   wire [31:0] 	   AD64_in   ;
+   
+   assign AD_in[0 ] = (AD_en[0 ] == oe_act) ? 1'bx : AD[0 ] ;
+   assign AD_in[1 ] = (AD_en[1 ] == oe_act) ? 1'bx : AD[1 ] ;
+   assign AD_in[2 ] = (AD_en[2 ] == oe_act) ? 1'bx : AD[2 ] ;
+   assign AD_in[3 ] = (AD_en[3 ] == oe_act) ? 1'bx : AD[3 ] ;
+   assign AD_in[4 ] = (AD_en[4 ] == oe_act) ? 1'bx : AD[4 ] ;
+   assign AD_in[5 ] = (AD_en[5 ] == oe_act) ? 1'bx : AD[5 ] ;
+   assign AD_in[6 ] = (AD_en[6 ] == oe_act) ? 1'bx : AD[6 ] ;
+   assign AD_in[7 ] = (AD_en[7 ] == oe_act) ? 1'bx : AD[7 ] ;
+   assign AD_in[8 ] = (AD_en[8 ] == oe_act) ? 1'bx : AD[8 ] ;
+   assign AD_in[9 ] = (AD_en[9 ] == oe_act) ? 1'bx : AD[9 ] ;
+   assign AD_in[10] = (AD_en[10] == oe_act) ? 1'bx : AD[10] ;
+   assign AD_in[11] = (AD_en[11] == oe_act) ? 1'bx : AD[11] ;
+   assign AD_in[12] = (AD_en[12] == oe_act) ? 1'bx : AD[12] ;
+   assign AD_in[13] = (AD_en[13] == oe_act) ? 1'bx : AD[13] ;
+   assign AD_in[14] = (AD_en[14] == oe_act) ? 1'bx : AD[14] ;
+   assign AD_in[15] = (AD_en[15] == oe_act) ? 1'bx : AD[15] ;
+   assign AD_in[16] = (AD_en[16] == oe_act) ? 1'bx : AD[16] ;
+   assign AD_in[17] = (AD_en[17] == oe_act) ? 1'bx : AD[17] ;
+   assign AD_in[18] = (AD_en[18] == oe_act) ? 1'bx : AD[18] ;
+   assign AD_in[19] = (AD_en[19] == oe_act) ? 1'bx : AD[19] ;
+   assign AD_in[20] = (AD_en[20] == oe_act) ? 1'bx : AD[20] ;
+   assign AD_in[21] = (AD_en[21] == oe_act) ? 1'bx : AD[21] ;
+   assign AD_in[22] = (AD_en[22] == oe_act) ? 1'bx : AD[22] ;
+   assign AD_in[23] = (AD_en[23] == oe_act) ? 1'bx : AD[23] ;
+   assign AD_in[24] = (AD_en[24] == oe_act) ? 1'bx : AD[24] ;
+   assign AD_in[25] = (AD_en[25] == oe_act) ? 1'bx : AD[25] ;
+   assign AD_in[26] = (AD_en[26] == oe_act) ? 1'bx : AD[26] ;
+   assign AD_in[27] = (AD_en[27] == oe_act) ? 1'bx : AD[27] ;
+   assign AD_in[28] = (AD_en[28] == oe_act) ? 1'bx : AD[28] ;
+   assign AD_in[29] = (AD_en[29] == oe_act) ? 1'bx : AD[29] ;
+   assign AD_in[30] = (AD_en[30] == oe_act) ? 1'bx : AD[30] ;
+   assign AD_in[31] = (AD_en[31] == oe_act) ? 1'bx : AD[31] ;
 
-wire    [31:0]  AD_in   ;
-assign AD_in[0 ] = (AD_en[0 ] == oe_act) ? 1'bx : AD[0 ] ;
-assign AD_in[1 ] = (AD_en[1 ] == oe_act) ? 1'bx : AD[1 ] ;
-assign AD_in[2 ] = (AD_en[2 ] == oe_act) ? 1'bx : AD[2 ] ;
-assign AD_in[3 ] = (AD_en[3 ] == oe_act) ? 1'bx : AD[3 ] ;
-assign AD_in[4 ] = (AD_en[4 ] == oe_act) ? 1'bx : AD[4 ] ;
-assign AD_in[5 ] = (AD_en[5 ] == oe_act) ? 1'bx : AD[5 ] ;
-assign AD_in[6 ] = (AD_en[6 ] == oe_act) ? 1'bx : AD[6 ] ;
-assign AD_in[7 ] = (AD_en[7 ] == oe_act) ? 1'bx : AD[7 ] ;
-assign AD_in[8 ] = (AD_en[8 ] == oe_act) ? 1'bx : AD[8 ] ;
-assign AD_in[9 ] = (AD_en[9 ] == oe_act) ? 1'bx : AD[9 ] ;
-assign AD_in[10] = (AD_en[10] == oe_act) ? 1'bx : AD[10] ;
-assign AD_in[11] = (AD_en[11] == oe_act) ? 1'bx : AD[11] ;
-assign AD_in[12] = (AD_en[12] == oe_act) ? 1'bx : AD[12] ;
-assign AD_in[13] = (AD_en[13] == oe_act) ? 1'bx : AD[13] ;
-assign AD_in[14] = (AD_en[14] == oe_act) ? 1'bx : AD[14] ;
-assign AD_in[15] = (AD_en[15] == oe_act) ? 1'bx : AD[15] ;
-assign AD_in[16] = (AD_en[16] == oe_act) ? 1'bx : AD[16] ;
-assign AD_in[17] = (AD_en[17] == oe_act) ? 1'bx : AD[17] ;
-assign AD_in[18] = (AD_en[18] == oe_act) ? 1'bx : AD[18] ;
-assign AD_in[19] = (AD_en[19] == oe_act) ? 1'bx : AD[19] ;
-assign AD_in[20] = (AD_en[20] == oe_act) ? 1'bx : AD[20] ;
-assign AD_in[21] = (AD_en[21] == oe_act) ? 1'bx : AD[21] ;
-assign AD_in[22] = (AD_en[22] == oe_act) ? 1'bx : AD[22] ;
-assign AD_in[23] = (AD_en[23] == oe_act) ? 1'bx : AD[23] ;
-assign AD_in[24] = (AD_en[24] == oe_act) ? 1'bx : AD[24] ;
-assign AD_in[25] = (AD_en[25] == oe_act) ? 1'bx : AD[25] ;
-assign AD_in[26] = (AD_en[26] == oe_act) ? 1'bx : AD[26] ;
-assign AD_in[27] = (AD_en[27] == oe_act) ? 1'bx : AD[27] ;
-assign AD_in[28] = (AD_en[28] == oe_act) ? 1'bx : AD[28] ;
-assign AD_in[29] = (AD_en[29] == oe_act) ? 1'bx : AD[29] ;
-assign AD_in[30] = (AD_en[30] == oe_act) ? 1'bx : AD[30] ;
-assign AD_in[31] = (AD_en[31] == oe_act) ? 1'bx : AD[31] ;
+   wire [3:0] 	   CBE_out ;
+   wire [3:0] 	   CBE_en ;
+   wire [3:0] 	   CBE_in  ;
 
-wire    [3:0]   CBE_out ;
-wire    [3:0]   CBE_en ;
-wire    [3:0]   CBE_in  ;
-assign CBE_in[3] = (CBE_en[3] == oe_act) ? 1'bx : CBE[3] ;
-assign CBE_in[2] = (CBE_en[2] == oe_act) ? 1'bx : CBE[2] ;
-assign CBE_in[1] = (CBE_en[1] == oe_act) ? 1'bx : CBE[1] ;
-assign CBE_in[0] = (CBE_en[0] == oe_act) ? 1'bx : CBE[0] ;
+   wire [3:0] 	   CBE64_out ;
+   wire [3:0] 	   CBE64_en ;
+   wire [3:0] 	   CBE64_in  ;
+   
+   assign CBE_in[3] = (CBE_en[3] == oe_act) ? 1'bx : CBE[3] ;
+   assign CBE_in[2] = (CBE_en[2] == oe_act) ? 1'bx : CBE[2] ;
+   assign CBE_in[1] = (CBE_en[1] == oe_act) ? 1'bx : CBE[1] ;
+   assign CBE_in[0] = (CBE_en[0] == oe_act) ? 1'bx : CBE[0] ;
+   
+   wire            RST_in = RST ;
+   wire            RST_out ;
+   wire            RST_en ;
+   
+   wire            INTA_en ;
+   wire            INTA_out ;
+   wire            INTA_in = INTA  ;
+   
+   wire            REQ_en ;
+   wire            REQ_out ;
 
-wire            RST_in = RST ;
-wire            RST_out ;
-wire            RST_en ;
+   wire 	   REQ64_en ;
+   wire 	   REQ64_out;
+   wire            REQ64_in = REQ64 ;
+   
+   wire            FRAME_out ;
+   wire            FRAME_en ;
+   wire            FRAME_in = FRAME ;
 
-wire            INTA_en ;
-wire            INTA_out ;
-wire            INTA_in = (INTA_en == oe_act) ? 1'bx : INTA  ;
+   wire 	   ACK64_out;
+   wire 	   ACK64_en;
+   wire 	   ACK64_in = ACK64;
+   
+   wire            IRDY_out ;
+   wire            IRDY_en ;
+   wire            IRDY_in = IRDY ;
+   
+   wire            DEVSEL_out ;
+   wire            DEVSEL_en ;
+   wire            DEVSEL_in = DEVSEL ;
+   
+   wire            TRDY_out ;
+   wire            TRDY_en ;
+   wire            TRDY_in = TRDY ;
+   
+   wire            STOP_out ;
+   wire            STOP_en ;
+   wire            STOP_in = STOP ;
+   
+   wire            PAR_out ;
+   wire            PAR_en ;
+   wire            PAR_in = PAR ;
 
-wire            REQ_en ;
-wire            REQ_out ;
-
-wire            FRAME_out ;
-wire            FRAME_en ;
-wire            FRAME_in = (FRAME_en == oe_act) ? 1'bx : FRAME ;
-
-wire            IRDY_out ;
-wire            IRDY_en ;
-wire            IRDY_in = (IRDY_en == oe_act) ? 1'bx : IRDY ;
-
-wire            DEVSEL_out ;
-wire            DEVSEL_en ;
-wire            DEVSEL_in = (DEVSEL_en == oe_act) ? 1'bx : DEVSEL ;
-
-wire            TRDY_out ;
-wire            TRDY_en ;
-wire            TRDY_in = (TRDY_en == oe_act) ? 1'bx : TRDY ;
-
-wire            STOP_out ;
-wire            STOP_en ;
-wire            STOP_in = (STOP_en == oe_act) ? 1'bx : STOP ;
-
-wire            PAR_out ;
-wire            PAR_en ;
-wire            PAR_in = (PAR_en == oe_act) ? 1'bx : PAR ;
-
-wire            PERR_out ;
-wire            PERR_en ;
-wire            PERR_in = (PERR_en == oe_act) ? 1'bx : PERR ;
-
-wire            SERR_out ;
-wire            SERR_en ;
-
+   wire            PAR64_out ;
+   wire            PAR64_en ;
+   wire            PAR64_in = PAR64 ;
+   
+   wire            PERR_out ;
+   wire            PERR_en ;
+   wire            PERR_in = PERR ;
+   
+   wire            SERR_out ;
+   wire            SERR_en ;
+   
    /*AUTOWIRE*/
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
    wire [31:0]		addr;			// From bridge of pci_bridge32.v
@@ -190,15 +207,25 @@ pci_bridge32 bridge
     // protocol pins
     .pci_frame_i   ( FRAME_in),
     .pci_frame_o   ( FRAME_out ),
-                    
     .pci_frame_oe_o( FRAME_en ),
+ 
+    .pci_req64_i   ( REQ64_in),
+    .pci_req64_o   ( REQ64_out ),
+    .pci_req64_oe_o( REQ64_en ),
+
+    .pci_ack64_i   ( ACK64_in),
+    .pci_ack64_o   ( ACK64_out ),
+    .pci_ack64_oe_o( ACK64_en ),
+
     .pci_irdy_oe_o ( IRDY_en ),
     .pci_devsel_oe_o( DEVSEL_en ),
     .pci_trdy_oe_o ( TRDY_en ),
     .pci_stop_oe_o ( STOP_en ),
     .pci_ad_oe_o   (AD_en),
+    .pci_ad64_oe_o   (AD64_en),
     .pci_cbe_oe_o  ( CBE_en) ,
-                    
+    .pci_cbe64_oe_o  ( CBE64_en) ,
+ 
     .pci_irdy_i    ( IRDY_in ),
     .pci_irdy_o    ( IRDY_out ),
                     
@@ -216,15 +243,23 @@ pci_bridge32 bridge
     // data transfer pins
     .pci_ad_i(AD_in),
     .pci_ad_o(AD_out),
-              
+    .pci_ad64_i(AD64_in),
+    .pci_ad64_o(AD64_out),
+ 
     .pci_cbe_i( CBE_in ),
     .pci_cbe_o( CBE_out ),
-
+    .pci_cbe64_i( CBE64_in ),
+    .pci_cbe64_o( CBE64_out ),
+ 
     // parity generation and checking pins
     .pci_par_i    ( PAR_in ),
     .pci_par_o    ( PAR_out ),
     .pci_par_oe_o ( PAR_en ),
-                  
+
+    .pci_par64_i    ( PAR64_in ),
+    .pci_par64_o    ( PAR64_out ),
+    .pci_par64_oe_o ( PAR64_en ),
+ 
     .pci_perr_i   ( PERR_in ),
     .pci_perr_o   ( PERR_out ),
     .pci_perr_oe_o( PERR_en ),
@@ -386,6 +421,86 @@ bufif0 SERR_buf     ( SERR, SERR_out, SERR_en ) ;
 			  .m_data_vld		(m_data_vld),
 			  .m_addr_n		(m_addr_n),
 			  .csr			(csr[39:0]));
+
+   assign AD64_in[0 ] = (AD64_en[0 ] == oe_act) ? 1'bx : AD64[0 ] ;
+   assign AD64_in[1 ] = (AD64_en[1 ] == oe_act) ? 1'bx : AD64[1 ] ;
+   assign AD64_in[2 ] = (AD64_en[2 ] == oe_act) ? 1'bx : AD64[2 ] ;
+   assign AD64_in[3 ] = (AD64_en[3 ] == oe_act) ? 1'bx : AD64[3 ] ;
+   assign AD64_in[4 ] = (AD64_en[4 ] == oe_act) ? 1'bx : AD64[4 ] ;
+   assign AD64_in[5 ] = (AD64_en[5 ] == oe_act) ? 1'bx : AD64[5 ] ;
+   assign AD64_in[6 ] = (AD64_en[6 ] == oe_act) ? 1'bx : AD64[6 ] ;
+   assign AD64_in[7 ] = (AD64_en[7 ] == oe_act) ? 1'bx : AD64[7 ] ;
+   assign AD64_in[8 ] = (AD64_en[8 ] == oe_act) ? 1'bx : AD64[8 ] ;
+   assign AD64_in[9 ] = (AD64_en[9 ] == oe_act) ? 1'bx : AD64[9 ] ;
+   assign AD64_in[10] = (AD64_en[10] == oe_act) ? 1'bx : AD64[10] ;
+   assign AD64_in[11] = (AD64_en[11] == oe_act) ? 1'bx : AD64[11] ;
+   assign AD64_in[12] = (AD64_en[12] == oe_act) ? 1'bx : AD64[12] ;
+   assign AD64_in[13] = (AD64_en[13] == oe_act) ? 1'bx : AD64[13] ;
+   assign AD64_in[14] = (AD64_en[14] == oe_act) ? 1'bx : AD64[14] ;
+   assign AD64_in[15] = (AD64_en[15] == oe_act) ? 1'bx : AD64[15] ;
+   assign AD64_in[16] = (AD64_en[16] == oe_act) ? 1'bx : AD64[16] ;
+   assign AD64_in[17] = (AD64_en[17] == oe_act) ? 1'bx : AD64[17] ;
+   assign AD64_in[18] = (AD64_en[18] == oe_act) ? 1'bx : AD64[18] ;
+   assign AD64_in[19] = (AD64_en[19] == oe_act) ? 1'bx : AD64[19] ;
+   assign AD64_in[20] = (AD64_en[20] == oe_act) ? 1'bx : AD64[20] ;
+   assign AD64_in[21] = (AD64_en[21] == oe_act) ? 1'bx : AD64[21] ;
+   assign AD64_in[22] = (AD64_en[22] == oe_act) ? 1'bx : AD64[22] ;
+   assign AD64_in[23] = (AD64_en[23] == oe_act) ? 1'bx : AD64[23] ;
+   assign AD64_in[24] = (AD64_en[24] == oe_act) ? 1'bx : AD64[24] ;
+   assign AD64_in[25] = (AD64_en[25] == oe_act) ? 1'bx : AD64[25] ;
+   assign AD64_in[26] = (AD64_en[26] == oe_act) ? 1'bx : AD64[26] ;
+   assign AD64_in[27] = (AD64_en[27] == oe_act) ? 1'bx : AD64[27] ;
+   assign AD64_in[28] = (AD64_en[28] == oe_act) ? 1'bx : AD64[28] ;
+   assign AD64_in[29] = (AD64_en[29] == oe_act) ? 1'bx : AD64[29] ;
+   assign AD64_in[30] = (AD64_en[30] == oe_act) ? 1'bx : AD64[30] ;
+   assign AD64_in[31] = (AD64_en[31] == oe_act) ? 1'bx : AD64[31] ;
+
+   assign CBE64_in[3] = (CBE64_en[3] == oe_act) ? 1'bx : CBE64[3] ;
+   assign CBE64_in[2] = (CBE64_en[2] == oe_act) ? 1'bx : CBE64[2] ;
+   assign CBE64_in[1] = (CBE64_en[1] == oe_act) ? 1'bx : CBE64[1] ;
+   assign CBE64_in[0] = (CBE64_en[0] == oe_act) ? 1'bx : CBE64[0] ;
+
+   bufif0 AD64_buf0   ( AD64[0],  AD64_out[0], AD64_en[0]) ;
+   bufif0 AD64_buf1   ( AD64[1],  AD64_out[1], AD64_en[1]) ;
+   bufif0 AD64_buf2   ( AD64[2],  AD64_out[2], AD64_en[2]) ;
+   bufif0 AD64_buf3   ( AD64[3],  AD64_out[3], AD64_en[3]) ;
+   bufif0 AD64_buf4   ( AD64[4],  AD64_out[4], AD64_en[4]) ;
+   bufif0 AD64_buf5   ( AD64[5],  AD64_out[5], AD64_en[5]) ;
+   bufif0 AD64_buf6   ( AD64[6],  AD64_out[6], AD64_en[6]) ;
+   bufif0 AD64_buf7   ( AD64[7],  AD64_out[7], AD64_en[7]) ;
+   bufif0 AD64_buf8   ( AD64[8],  AD64_out[8], AD64_en[8]) ;
+   bufif0 AD64_buf9   ( AD64[9],  AD64_out[9], AD64_en[9]) ;
+   bufif0 AD64_buf10  ( AD64[10], AD64_out[10],AD64_en[10] ) ;
+   bufif0 AD64_buf11  ( AD64[11], AD64_out[11],AD64_en[11] ) ;
+   bufif0 AD64_buf12  ( AD64[12], AD64_out[12],AD64_en[12] ) ;
+   bufif0 AD64_buf13  ( AD64[13], AD64_out[13],AD64_en[13] ) ;
+   bufif0 AD64_buf14  ( AD64[14], AD64_out[14],AD64_en[14] ) ;
+   bufif0 AD64_buf15  ( AD64[15], AD64_out[15],AD64_en[15] ) ;
+   bufif0 AD64_buf16  ( AD64[16], AD64_out[16],AD64_en[16] ) ;
+   bufif0 AD64_buf17  ( AD64[17], AD64_out[17],AD64_en[17] ) ;
+   bufif0 AD64_buf18  ( AD64[18], AD64_out[18],AD64_en[18] ) ;
+   bufif0 AD64_buf19  ( AD64[19], AD64_out[19],AD64_en[19] ) ;
+   bufif0 AD64_buf20  ( AD64[20], AD64_out[20],AD64_en[20] ) ;
+   bufif0 AD64_buf21  ( AD64[21], AD64_out[21],AD64_en[21] ) ;
+   bufif0 AD64_buf22  ( AD64[22], AD64_out[22],AD64_en[22] ) ;
+   bufif0 AD64_buf23  ( AD64[23], AD64_out[23],AD64_en[23] ) ;
+   bufif0 AD64_buf24  ( AD64[24], AD64_out[24],AD64_en[24] ) ;
+   bufif0 AD64_buf25  ( AD64[25], AD64_out[25],AD64_en[25] ) ;
+   bufif0 AD64_buf26  ( AD64[26], AD64_out[26],AD64_en[26] ) ;
+   bufif0 AD64_buf27  ( AD64[27], AD64_out[27],AD64_en[27] ) ;
+   bufif0 AD64_buf28  ( AD64[28], AD64_out[28],AD64_en[28] ) ;
+   bufif0 AD64_buf29  ( AD64[29], AD64_out[29],AD64_en[29] ) ;
+   bufif0 AD64_buf30  ( AD64[30], AD64_out[30],AD64_en[30] ) ;
+   bufif0 AD64_buf31  ( AD64[31], AD64_out[31],AD64_en[31] ) ;
+   
+   bufif0 CBE64_buf0 ( CBE64[0], CBE64_out[0], CBE64_en[0] ) ;
+   bufif0 CBE64_buf1 ( CBE64[1], CBE64_out[1], CBE64_en[1] ) ;
+   bufif0 CBE64_buf2 ( CBE64[2], CBE64_out[2], CBE64_en[2] ) ;
+   bufif0 CBE64_buf3 ( CBE64[3], CBE64_out[3], CBE64_en[3] ) ;
+
+   bufif0 ACK64_buf  ( ACK64, ACK64_out, ACK64_en);
+   bufif0 REQ64_buf  ( REQ64, REQ64_out, REQ64_en);
+   bufif0 REQ64_buf  ( PAR64, PAR64_out, PAR64_en);
    
 endmodule // TOP
 
